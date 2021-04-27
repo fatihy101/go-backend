eb5fd9d3d15d389b477dbf51bce9096b78f88f88
Fatih <fatihy@yahoo.com>
2021-04-27 21:09:52 +0300

    bson name fields added.

--- db/types.go
+++ db/types.go
@@ -19,44 +19,44 @@ type Base struct {
 
 type UserRole struct {
 	Base  `bson:",inline"`
-	Email string `json:"email"`
-	Role  string `json:"role"`
+	Email string `json:"email" bson:"email"`
+	Role  string `json:"role" bson:"role"`
 }
 
 type Address struct {
 	Base        `bson:",inline"`
-	Title       string `json:"title"`
-	City        string `json:"city"`
-	State       string `json:"state"`
-	Zipcode     string `json:"zipcode"`
-	Street      string `json:"street"`
-	BuildingNo  string `json:"building_no"`
-	Description string `json:"description"`
+	Title       string `json:"title" bson:"title"`
+	City        string `json:"city" bson:"city"`
+	State       string `json:"state" bson:"state"`
+	Zipcode     string `json:"zipcode" bson:"zipcode"`
+	Street      string `json:"street" bson:"street"`
+	BuildingNo  string `json:"building_no" bson:"building_no"`
+	Description string `json:"description" bson:"description"`
 }
 
 type Client struct {
 	Base          `bson:",inline"`
 	BirthdayDate  time.Time
-	Email         string  `json:"email"`
-	Name          string  `json:"name"`
-	Surname       string  `json:"surname"`
-	PhoneNumber   string  `json:"phone_number"`
-	ImagePath     Image   `json:"image_path"`
-	ClientAddress Address `json:"client_address"`
+	Email         string  `json:"email" bson:"email"`
+	Name          string  `json:"name" bson:"name"`
+	Surname       string  `json:"surname" bson:"surname"`
+	PhoneNumber   string  `json:"phone_number" bson:"phone_number"`
+	ImagePath     Image   `json:"image_path" bson:"image_path"`
+	ClientAddress Address `json:"client_address" bson:"client_address"`
 }
 
 type Renter struct { // TODO: FUTURE Add geolocation on search from map.
 	Base           `bson:",inline"`
-	Name           string  `json:"name"`
-	Surname        string  `json:"surname"`
-	Email          string  `json:"email"`
-	StoreInfo      string  `json:"store_info"`
-	StoreName      string  `json:"store_name"`
-	PhoneNumber    string  `json:"phone_number"`
-	Rating         float32 `json:"rating"`
-	ProfilePicPath Image   `json:"profile_pic_path"`
-	HeaderPicPath  Image   `json:"header_pic_path"`
-	RenterAddress  Address `json:"renter_address"`
+	Name           string  `json:"name" bson:"name"`
+	Surname        string  `json:"surname" bson:"surname"`
+	Email          string  `json:"email" bson:"email"`
+	StoreInfo      string  `json:"store_info" bson:"store_info"`
+	StoreName      string  `json:"store_name" bson:"store_name"`
+	PhoneNumber    string  `json:"phone_number" bson:"phone_number"`
+	Rating         float32 `json:"rating" bson:"rating"`
+	ProfilePicPath Image   `json:"profile_pic_path" bson:"profile_pic_path"`
+	HeaderPicPath  Image   `json:"header_pic_path" bson:"header_pic_path"`
+	RenterAddress  Address `json:"renter_address" bson:"renter_address"`
 }
 
 type Order struct {
@@ -64,9 +64,9 @@ type Order struct {
 	Product      Product
 	Client       Client
 	Renter       Renter
-	DeliveryType string
+	DeliveryType string `json:"delivery_type" bson:"delivery_type"`
 	Address      Address
-	IsRental     bool
+	IsRental     bool `json:"is_rental" bson:"is_rental"`
 	InitalImages []Image
 }
 
