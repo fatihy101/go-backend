d42037f1be8ebac30f1245d026f9a943f9dd06c1
Fatih <fatihy@yahoo.com>
2021-04-28 14:50:39 +0300

    Flags added.

---
+++ flags/flags.go
@@ -0,0 +1,36 @@
+package flags
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Configuration struct {
+	SERVER_PORT string
+	CON_STR     string
+	DBNAME      string
+	JWT_KEY     string
+}
+
+var config Configuration
+
+func InitConfig() Configuration {
+	// Open our jsonFile
+	jsonFile, err := os.Open("config.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &config)
+	return config
+}
+
+func GetConfig() Configuration {
+	return config
+}
