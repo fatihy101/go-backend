5d9dafce2ee8ab6e204dc4a1ded54faa5a3b4fc3
Fatih <fatihy@yahoo.com>
2021-05-07 14:02:52 +0300

    Getting renter and client info is completed.

--- routes/client.go
+++ routes/client.go
@@ -1,11 +1,22 @@
 package routes
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func getClientInfo(w http.ResponseWriter, r *http.Request) {
+	role := r.Context().Value(UserRoleContext)
+	if role != ClientRole {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	email := r.Context().Value(UserEmailContext).(string)
 
+	mdb := getDB(r)
+	clientInfo := mdb.GetClientByEmail(r.Context(), email)
+
+	json.NewEncoder(w).Encode(clientInfo)
 }
 
 func updateClientInfo(w http.ResponseWriter, r *http.Request) {
--- routes/renter.go
+++ routes/renter.go
@@ -1,8 +1,22 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func getRenterInfo(w http.ResponseWriter, r *http.Request) {
+	role := r.Context().Value(UserRoleContext)
+	if role != RenterRole {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	email := r.Context().Value(UserEmailContext).(string)
+
+	mdb := getDB(r)
+	renterInfo := mdb.GetRenterByEmail(r.Context(), email)
+
+	json.NewEncoder(w).Encode(renterInfo)
 }
 
 func updateRenterInfo(w http.ResponseWriter, r *http.Request) {
